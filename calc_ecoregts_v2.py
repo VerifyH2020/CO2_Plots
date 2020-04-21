5fc02e32fe0b8ae776d67b50c2cf61a020d3919a
Matt McGrath <obfiscator@gmail.com>
2020-04-21 11:57:05 +0200

    Adding Vlad's script to calculate country totals from spatial NetCDF fiels

---
+++ calc_ecoregts_v2.py
@@ -0,0 +1,237 @@
+import numpy as np, netCDF4, os
+from mpl_toolkits.basemap import maskoceans
+from grid import Grid
+import sys
+
+######
+# python calc_ecoregts_v2.py PATH PATH_OUT
+#
+# PATH can be either a directory or a NetCDF file.  If a directory,
+# the script will try to run on every .nc file in the directory for
+# which an existing CountryTot file is not found.
+######
+
+# The purpose of this script is to take a spatially explicit NetCDF file
+# and calculate country totals, putting them into a new .nc file that
+# just has timeseries for every country.  The countries (and regions) are
+# found in the path_mask file, which is a NetCDF file with a list of
+# country (and region) names along with a mask showing which pixels belong
+# to which country (0 means no part of the country is in that pixel, 1 means
+# the pixel is fully contained in the country, and a number between 0 and 1
+# means that part of the pixel is found in that country/region).
+
+# The country masks are regridded to the resolution of the input file/files.
+# In order to save time, these masks are stored in the path_regs directory
+# with the grid, latitudinal, and longitudinal extent in the name.  For any
+# given input file, the regridding is only done if a file matching the
+# grid, latitudinal extent, and longitudinal extent is not found.
+
+try:
+   path=sys.argv[1]
+   path_out=sys.argv[2]
+except:
+   path=""
+#endtry
+if path == "":
+   print("Need to specify both an input path/file and a place to put the new files.")
+   print("python calc_ecoregts.py PATH PATH_OUT")
+   sys.exit()
+else:
+   print("**************************************************************")
+   print("Input path: " + path)
+   print("Output path: " + path_out)
+   print("**************************************************************")
+#endif
+
+# Notice the filename now includes the extents of the grid so that
+# the script can process the same size grid if they are different lat/lons
+# path to OLD country mask files regridded to different input resolutions
+#path_regs = "/home/dods/verify/VERIFY_INPUT/COUNTRY_MASKS/eurocomCountryMask%(EEZ)s_%(nlat)sx%(nlon)s_%(slat)s%(elat)s_%(slon)s%(elon)s.nc"
+# For the NEW, extended masks.  
+path_regs = "/home/dods/verify/VERIFY_INPUT/COUNTRY_MASKS/extended_eurocomCountryMask%(EEZ)s_%(nlat)sx%(nlon)s_%(slat)s%(elat)s_%(slon)s%(elon)s.nc"
+
+# path to OLD country mask file to be regridded to each input resolution
+#path_mask = "/home/surface5/mmcgrath/ORIGINAL_VERIFY_DATA_FILES/eurocomCountryMaskEEZ_0.1x0.1.nc"
+# This is for the NEW mask
+path_mask = "/home/dods/verify/VERIFY_INPUT/COUNTRY_MASKS/extended_country_region_masks_0.1x0.1.nc"
+
+# variable names to be processed as country means : CountryTot = sum(data * area * fraction) / sum(area * fraction)
+means = ["tas", "pr", "rsds", "mrso", "mrro", "evapotrans", "transpft", "landCoverFrac", "lai"]
+# variable names to be processed as country sums : CountryTot = sum(data)
+sums = ["CO2"]
+# all other variables are proccessed as country totals : CountryTot = sum(data * area * fraction)
+
+# possible netcdf variable names for time/lat/lon
+timenames = set(["time", "year"])
+latnames = set(["lat", "latitude", "south_north"])
+lonnames = set(["lon", "longitude", "west_east"])
+
+nc = netCDF4.Dataset(path_mask)
+clat = nc.variables["latitude"][:]
+clon = nc.variables["longitude"][:]
+cmask = nc.variables["country_mask"][:]
+cname = nc.variables["country_name"][:]
+ccode = nc.variables["country_code"][:]
+nc.close()
+cgrid = Grid(lat = clat, lon = clon)
+
+for item in [path] if path.endswith(".nc") else [os.path.join(path, filename) for filename in os.listdir(path)]:
+    if not item.endswith("2D.nc"): continue
+    print(item)
+    pathEEZ = os.path.join(path_out, os.path.split(item)[-1].replace("_2D", "_CountryTotWithEEZ"))
+    pathNoEEZ = os.path.join(path_out, os.path.split(item)[-1].replace("_2D", "_CountryTotWithOutEEZ"))
+    if os.path.exists(pathEEZ) or os.path.exists(pathNoEEZ):
+        print(pathEEZ, pathNoEEZ)
+        try:
+           if input("Files exist! Overwrite (y/n)? ").upper() != "Y": continue
+        except:
+           print("File exists!  Not overwriting.")
+           continue
+
+    nc = netCDF4.Dataset(item)
+    if len(timenames.intersection(nc.dimensions.keys())) == 0:
+        timedim = timevar = ""
+    else:
+        timedim = list(timenames.intersection(nc.dimensions.keys()))[0]
+        timevar = list(timenames.intersection(nc.variables.keys()))[0]
+    latdim = list(latnames.intersection(nc.dimensions.keys()))[0]
+    latvar = list(latnames.intersection(nc.variables.keys()))[0]
+    londim = list(lonnames.intersection(nc.dimensions.keys()))[0]
+    lonvar = list(lonnames.intersection(nc.variables.keys()))[0]
+    lat = nc.variables[latvar][:]
+    lon = nc.variables[lonvar][:]
+    nlat = len(lat)
+    nlon = len(lon)
+    print("%sx%s" % (nlat, nlon))
+    slat=lat[0]
+    elat=lat[-1]
+    slon=lon[0]
+    elon=lon[-1]
+    if slat < 0.0:
+       slat="{:.2f}S".format(abs(slat))
+    else:
+       slat="{:.2f}N".format(slat)
+    #endif
+    if elat < 0.0:
+       elat="{:.2f}S".format(abs(elat))
+    else:
+       elat="{:.2f}N".format(elat)
+    #endif
+    if slon < 0.0:
+       slon="{:.2f}W".format(abs(slon))
+    else:
+       slon="{:.2f}E".format(slon)
+    #endif
+    if elon < 0.0:
+       elon="{:.2f}W".format(abs(elon))
+    else:
+       elon="{:.2f}E".format(elon)
+    #endif
+
+    print("Spatial extent: ",slat,elat,slon,elon)
+
+    if not os.path.exists(path_regs % dict(nlat=nlat, nlon=nlon,slat=slat,elat=elat,slon=slon,elon=elon,EEZ='EEZ')):
+        grid = Grid(lat = lat, lon = lon)
+        cgrid.setRegrid(grid)
+        newmask = cgrid.regrid(cmask)
+
+        ncreg = netCDF4.Dataset(path_regs % dict(nlat=nlat, nlon=nlon,slat=slat,elat=elat,slon=slon,elon=elon,EEZ='EEZ'), "w")
+        ncreg.createDimension("lon", nlon)
+        ncreg.createDimension("lat", nlat)
+        ncreg.createDimension("country", len(cmask))
+        ncreg.createDimension("strlength", 50)
+        ncreg.createVariable("country_code", "S1", ("country", "strlength"))
+        ncreg.createVariable("country_name", "S1", ("country", "strlength"))
+        ncreg.createVariable("lon", "f4", ("lon",))
+        ncreg.createVariable("lat", "f4", ("lat",))
+        ncreg.createVariable("area", "f4", ("lat", "lon"))
+        ncreg.createVariable("country_mask", "f4", ("country", "lat", "lon"))
+        ncreg.variables["lon"][:] = lon
+        ncreg.variables["lat"][:] = lat
+        ncreg.variables["area"][:] = grid.area
+        ncreg.variables["country_mask"][:] = newmask.filled(0)
+        for idx in range(len(cmask)): 
+            for jdx in range(len(ccode[idx])):
+                if ccode[idx, jdx] is not np.ma.masked:
+                    ncreg.variables["country_code"][idx, jdx] = ccode[idx, jdx]
+            for jdx in range(len(cname[idx])):
+                if cname[idx, jdx] is not np.ma.masked:
+                    ncreg.variables["country_name"][idx, jdx] = cname[idx, jdx]
+        ncreg.close()
+
+    ncreg = netCDF4.Dataset(path_regs % dict(nlat=nlat, nlon=nlon,slat=slat,elat=elat,slon=slon,elon=elon,EEZ='EEZ'))
+    reglat = ncreg.variables["lat"][:]
+    reglon = ncreg.variables["lon"][:]
+    regmaskEEZ = ncreg.variables["country_mask"][:].filled(0)
+    regcode = ncreg.variables["country_code"][:]
+    regname = ncreg.variables["country_name"][:]
+    regarea = ncreg.variables["area"][:]
+    ncreg.close()
+
+    # create regmask copy with filtered ocean pixels
+    reglons, reglats = np.meshgrid(reglon, reglat)
+    regmaskNoEEZ = np.zeros_like(regmaskEEZ)
+    for idx in range(len(regmaskEEZ)):
+        countryname=b"".join([letter for letter in regname[idx] if letter is not np.ma.masked])
+        print("Making mask without EEZ for {}".format(countryname))
+        regmaskNoEEZ[idx] = maskoceans(reglons, reglats, regmaskEEZ[idx], inlands = False, resolution = "f", grid = 1.25).filled(0)
+
+    if not np.allclose(lat, reglat) or not np.allclose(lon, reglon): raise Exception
+
+    for newpath, regmask in zip([pathEEZ, pathNoEEZ], [regmaskEEZ, regmaskNoEEZ]):
+        print("Create", newpath)
+        ncout = netCDF4.Dataset(newpath, "w")
+        ncout.createDimension("country", len(regmask))
+        if timedim != "": ncout.createDimension(timedim, None)
+        ncout.createDimension("strlength", 50)
+        for dim in set(nc.dimensions.keys()).difference([latdim, londim, timedim]):
+            ncout.createDimension(dim, nc.dimensions[dim].size)
+
+        ncout.createVariable("country_code", "S1", ("country", "strlength"))
+        ncout.createVariable("country_name", "S1", ("country", "strlength"))
+        if timevar != "":
+            ncout.createVariable(timevar, nc.variables[timevar].dtype, (timedim,))
+            ncout.variables[timevar].setncatts(nc.variables[timevar].__dict__)
+            ncout.variables[timevar][:] = nc.variables[timevar][:]
+
+        for var in set(nc.variables.keys()).difference([latvar, lonvar, timevar]):
+            if var.startswith(lonvar) or var.startswith(latvar): continue
+            print(var)
+            if not nc.variables[var].dimensions[-2:] == tuple([latdim, londim]):
+                ncout.createVariable(var, nc.variables[var].dtype, nc.variables[var].dimensions)
+                ncout.variables[var].setncatts(nc.variables[var].__dict__)
+                ncout.variables[var][:] = nc.variables[var][:]
+            else:
+                ncout.createVariable(var, "f4", tuple(list(nc.variables[var].dimensions[:-2]) + ["country"]))
+                for key,value in nc.variables[var].__dict__.items():
+                    if key in ["_FillValue", "missing_value"]: continue
+                    if key == "units" and var not in means: wvalue = value.replace("m-2", "").replace("m2", "").replace("m^2", "").replace("m^-2", "").replace("  "," ").replace("//","/").strip()
+                    elif key == "longname" or key == "long_name":
+                        if var in means: wvalue = value + " (country mean)"
+                        elif var in sums: wvalue = value + " (country sum)"
+                        else: wvalue = value + " (country total)"
+                        print(wvalue)
+                    else: wvalue = value
+                    ncout.variables[var].setncattr(key, wvalue)
+
+                data = np.ma.masked_invalid(nc.variables[var][:])
+                print(data.shape, data.min(), data.max())
+
+                for idx in range(len(regmask)):
+                    print(b"".join([letter for letter in regname[idx] if letter is not np.ma.masked]))
+                    ncout.variables["country_code"][idx] = regcode[idx]
+                    ncout.variables["country_name"][idx] = regname[idx]
+                    if var in sums:
+                        ncout.variables[var][:,idx] = (data * regmask[idx]).sum(axis=(-1,-2))
+                    else:
+                        if len(data.shape) < 4:
+                            if var in means: ncout.variables[var][...,idx] = (data * regmask[idx] * regarea).sum(axis=(-1,-2)) / np.ma.where(data.mask == False, regmask[idx] * regarea, 0).sum(axis=(-1,-2))
+                            else: ncout.variables[var][...,idx] = (data * regmask[idx] * regarea).sum(axis=(-1,-2))
+                        else:
+                            for jdx in range(data.shape[1]):
+                                print(jdx)
+                                if var in means: ncout.variables[var][:,jdx,idx] = (data[:,jdx,:,:] * regmask[idx] * regarea).sum(axis=(-1,-2)) / np.ma.where(data[:,jdx,:,:].mask == False, regmask[idx] * regarea, 0).sum(axis=(-1,-2))
+                                else: ncout.variables[var][:,jdx,idx] = (data[:,jdx,:,:] * regmask[idx] * regarea).sum(axis=(-1,-2))
+        ncout.close()
+    nc.close()
+
---
+++ grid.py
@@ -0,0 +1,154 @@
+# Class for grid settings and plotting maps on the grid
+# Latitudes are at [90, -90] with decreasing order
+# Longitudes are at [-180, 180] with increasing order
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+
+rainbow = {"red"   : ((0., 0.53, 0.53), (0.077, 0.69, 0.69), (0.154, 0.84, 0.84), (0.231, 0.1,  0.1),  (0.308, 0.32, 0.32), (0.385, 0.48, 0.48), (0.462, 0.3,  0.3),  (0.539, 0.56, 0.56), (0.616, 0.79,  0.79),  (0.693, 0.965, 0.965), (0.77, 0.96, 0.96), (0.847, 0.94, 0.94), (0.924, 0.91,  0.91),  (1., 0.86, 0.86)),
+           "green" : ((0., 0.18, 0.28), (0.077, 0.47, 0.47), (0.154, 0.75, 0.75), (0.231, 0.39, 0.39), (0.308, 0.54, 0.54), (0.385, 0.68, 0.68), (0.462, 0.7,  0.7),  (0.539, 0.79, 0.79), (0.616, 0.875, 0.875), (0.693, 0.93,  0.93),  (0.77, 0.75, 0.75), (0.847, 0.57, 0.57), (0.924, 0.375, 0.375), (1., 0.02, 0.02)),
+           "blue"  : ((0., 0.45, 0.45), (0.077, 0.65, 0.65), (0.154, 0.87, 0.87), (0.231, 0.69, 0.69), (0.308, 0.78, 0.78), (0.385, 0.87, 0.87), (0.462, 0.39, 0.39), (0.539, 0.53, 0.53), (0.616, 0.67,  0.67),  (0.693, 0.33,  0.33),  (0.77, 0.25, 0.25), (0.847, 0.18, 0.18), (0.924, 0.11,  0.11),  (1., 0.05, 0.05))}
+
+diverge = {"red"   : ((0., 0.24, 0.24), (0.1, 0.23, 0.23), (0.2, 0.46, 0.46), (0.3, 0.7,  0.7),  (0.4, 0.9,  0.9),  (0.5, 1.0,  1.0),  (0.6, 1.0,  1.0),  (0.7, 0.97, 0.97), (0.8, 0.93, 0.93), (0.9, 0.82, 0.82), (1., 0.7,  0.7)),
+           "green" : ((0., 0.32, 0.32), (0.1, 0.54, 0.54), (0.2, 0.71, 0.71), (0.3, 0.86, 0.86), (0.4, 0.96, 0.96), (0.5, 0.98, 0.98), (0.6, 0.89, 0.89), (0.7, 0.74, 0.74), (0.8, 0.53, 0.53), (0.9, 0.3,  0.3),  (1., 0.11, 0.11)),
+           "blue"  : ((0., 0.63, 0.63), (0.1, 0.79, 0.79), (0.2, 0.89, 0.89), (0.3, 0.96, 0.96), (0.4, 0.99, 0.99), (0.5, 0.82, 0.82), (0.6, 0.66, 0.66), (0.7, 0.49, 0.49), (0.8, 0.34, 0.34), (0.9, 0.24, 0.24), (1., 0.24, 0.24))}
+
+RE = 6.371229E6 # Earth radius
+
+def getStep(minval, maxval):
+    steps = np.array([5, 10, 20, 30, 60])
+    idx = np.searchsorted(steps, (maxval-minval)/5.)
+    idx = min(idx, len(steps)-1)
+    return steps[idx]
+
+# Get boundes for grid cells (mid-between the nodes, dimension one larger than that of nodes)
+def getBounds(nodes):
+    bounds = (nodes[1:] + nodes[:-1])/2.
+    bound0 = nodes[0] - (nodes[1]-nodes[0])/2.
+    boundN = nodes[-1] + (nodes[-1]-nodes[-2])/2.
+    return np.append(np.append(bound0, bounds), boundN)
+
+# Get min/max bounds of grid cells from bounds array
+def getMinMaxBnd(bounds):
+    minbnd = bounds[:-1] if bounds[-1] > bounds[0] else bounds[1:]
+    maxbnd = bounds[1:] if bounds[-1] > bounds[0] else bounds[:-1]
+    return minbnd, maxbnd
+
+class Grid:
+
+    # Grid can be initialized in two ways:
+    # - by grid dimensions (nlat/nlon) => regular grid is set to fit in
+    # - by grid nodes (lat/lon) => grid cell bounds are adapted to fit in
+    def __init__(self, nlat = None, nlon = None, lat = None, lon = None):
+        if nlat is not None and nlon is not None:
+            self.nlat = nlat
+            self.nlon = nlon
+            lat_step = 180./nlat
+            lon_step = 360./nlon
+            self.lat = np.arange(  90 - lat_step/2., -90, -lat_step)
+            self.lon = np.arange(-180 + lon_step/2., 180, lon_step)
+        if lat is not None and lon is not None:
+            self.nlat = len(lat)
+            self.nlon = len(lon)
+            self.lat = np.clip(lat, -90, 90)
+            self.lon = np.clip(lon, -180, 180)
+        self.meshlat = np.clip(getBounds(self.lat), -90, 90)
+        self.meshlon = np.clip(getBounds(self.lon), -180, 180)
+        self.dlat = np.abs(self.meshlat[1:] - self.meshlat[:-1])
+        self.dlon = np.abs(self.meshlon[1:] - self.meshlon[:-1])
+        self.lonmin, self.lonmax = getMinMaxBnd(self.meshlon)
+        self.latmin, self.latmax = getMinMaxBnd(self.meshlat)
+        self.meshlon, self.meshlat = np.meshgrid(self.meshlon, self.meshlat)
+        self.area = 2 * RE**2 * self.dlon[None,:] * np.pi/180 * np.sin(0.5 * self.dlat[:,None] * np.pi/180) * np.cos(self.lat[:,None] * np.pi/180)
+        self.dx = self.dlon[None,:]/360 * 2*np.pi*RE * np.cos(self.lat[:,None] * np.pi/180)
+        self.dy = self.dlat[:,None]/180 * np.pi*RE * np.ones_like(self.dlon[None,:])
+        self.contfrac = 1
+
+    def meanmask(self, data, mask = 1, dosum = False):
+        if hasattr(data, "mask"): loc_area = np.where(data.mask == False, self.area * self.contfrac * mask, 0)
+        else: loc_area = self.area * self.contfrac * mask
+        if dosum == False: out = (data * loc_area).sum(axis=-1).sum(axis=-1) / loc_area.sum(axis=-1).sum(axis=-1)
+        else: out = (data * loc_area).sum(axis=-1).sum(axis=-1)
+        return out
+
+    def plotmap(self, data, ptype = "map", title = "", ltitle = "", rtitle = "", info = None, filename = "test.png", dpi = 125,
+                drawcoast = True, drawgrid = True, levels = 20, vmin = None, vmax = None, bounds = None, ticks = None, zoom = dict(north = 90, south = -90, west = -180, east = 180)):
+        delta = 3
+        north = min( 90, max(self.lat) + delta, zoom["north"] + delta)
+        south = max(-90, min(self.lat) - delta, zoom["south"] - delta)
+        west = max(-180, min(self.lon) - delta, zoom["west"] - delta)
+        east = min( 180, max(self.lon) + delta, zoom["east"] + delta)
+        m = Basemap(projection = "mill", llcrnrlat = south, urcrnrlat = north, llcrnrlon = west, urcrnrlon = east, resolution = "c")
+        if drawcoast: m.drawcoastlines(linewidth=0.5)
+        if drawgrid:
+            if north-south > 150: parallels = [-80, -60, -30, 0, 30, 60, 80]
+            else: parallels = np.arange(-90, 91, getStep(south, north))
+            m.drawparallels(parallels, labels = [1, 0, 1, 1])
+            m.drawmeridians(np.arange(-180, 181, getStep(west, east)), labels = [0, 0, 0, 1])
+        if ptype == "map":
+            cmap = matplotlib.colors.LinearSegmentedColormap("my_colormap", rainbow, levels)
+        elif ptype == "dif":
+            cmap = matplotlib.colors.LinearSegmentedColormap("my_colormap", diverge, levels)
+            lim = max(abs(np.percentile(data.compressed(), 1)), abs(np.percentile(data.compressed(), 99))) if type(data) is np.ma.masked_array else max(abs(np.percentile(data, 1)), abs(np.percentile(data, 99)))
+            vmin = -lim
+            vmax = lim
+        norm = None if bounds is None else matplotlib.colors.BoundaryNorm(boundaries=bounds, ncolors=levels)
+        if bounds is not None: vmin = vmax = None
+        o = (self.meshlat <= north) & (self.meshlat >= south) & (self.meshlon >= west) & (self.meshlon <= east)
+        inorth = np.where(self.lat <= north)[0][0]
+        isouth = np.where(self.lat >= south)[0][-1]
+        iwest = np.where(self.lon >= west)[0][0]
+        ieast = np.where(self.lon <= east)[0][-1]
+        cmesh = m.pcolormesh(self.meshlon[inorth:isouth+2,iwest:ieast+2], self.meshlat[inorth:isouth+2,iwest:ieast+2], data[inorth:isouth+1,iwest:ieast+1],
+                             shading = "flat", cmap = cmap, latlon = True, vmin = vmin, vmax = vmax, norm = norm)
+        cbar = m.colorbar(cmesh, pad = 0.08, location = "right")
+        if bounds is not None: cbar.set_ticks(bounds)
+        if ticks is not None: cbar.set_ticks(ticks)
+        if info is not None: plt.annotate(info, xy = (0.05, 0.05), xycoords = "axes fraction")
+        if title != "": plt.title(title)
+        if ltitle != "": plt.title(ltitle, loc = "left")
+        if rtitle != "": plt.title(rtitle, loc = "right")
+        plt.savefig(filename, dpi = dpi)
+        plt.clf()
+
+    def setRegrid(self, grid):
+        print("Building regrid...")
+        self.reg = np.empty((grid.nlat, grid.nlon), dtype = object)
+        for i in range(grid.nlat):
+            for j in range(grid.nlon):
+                self.reg[i,j] = []
+                ns, ms = np.where( ( ((self.latmin >= grid.latmin[i]) & (self.latmin <= grid.latmax[i])) | ((self.latmax >= grid.latmin[i]) & (self.latmax <= grid.latmax[i])) | ((self.latmin <= grid.latmin[i]) & (self.latmax >= grid.latmax[i])) )[:,None] &
+                                   ( ((self.lonmin >= grid.lonmin[j]) & (self.lonmin <= grid.lonmax[j])) | ((self.lonmax >= grid.lonmin[j]) & (self.lonmax <= grid.lonmax[j])) | ((self.lonmin <= grid.lonmin[j]) & (self.lonmax >= grid.lonmax[j])) )[None,:] )
+                for n, m in zip(ns, ms):
+                    lon1 = max(grid.lonmin[j], self.lonmin[m])
+                    lon2 = min(grid.lonmax[j], self.lonmax[m])
+                    lat1 = max(grid.latmin[i], self.latmin[n])
+                    lat2 = min(grid.latmax[i], self.latmax[n])
+                    subarea = 2 * RE**2 * (lon2-lon1) * np.pi/180 * np.sin(0.5 * (lat2-lat1) * np.pi/180) * np.cos((lat2+lat1)/2. * np.pi/180)
+                    if subarea > 0: self.reg[i,j].append((n,m,subarea))
+                    if subarea < 0: raise Exception("ERROR")
+        self.regtask = "recalc %sx%s to %sx%s" % (self.nlat, self.nlon, grid.nlat, grid.nlon)
+        print("Regrid has been set : %s" % self.regtask)
+
+    def regrid(self, data, add_masked_area = False):
+        if len(data.shape) == 3:
+            nlat, nlon = self.reg.shape
+            ntime = data.shape[0]
+            newdata = np.ma.array(np.zeros((ntime, nlat, nlon)), mask=True)
+            for k in range(ntime):
+                print(k)
+                for i in range(nlat):
+                    for j in range(nlon):
+                        totarea = 0
+                        for n,m,subarea in self.reg[i,j]:
+                            if data[k,n,m] is not np.ma.masked:
+                                if newdata[k,i,j] is np.ma.masked: newdata[k,i,j] = 0
+                                newdata[k,i,j] += data[k,n,m] * subarea
+                            if data[k,n,m] is not np.ma.masked or add_masked_area:
+                                totarea += subarea
+                        if newdata[k,i,j] is not np.ma.masked: newdata[k,i,j] /= totarea
+            return newdata
+
